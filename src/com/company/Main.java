b8afd120e448d9df9882764bc7f9f5afb5646914
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-10 15:53:56 +0200

    Add Try to Connect to the server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,16 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
     }
+    public static void Connection () {
+        try(ServerSocket serverSocket = new ServerSocket(2000)) {
+            Socket connSocket = serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
