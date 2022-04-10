fa26473ea3594ec7e6c0a3446b623b78fd3a2b4d
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-10 15:50:42 +0200

    Add imports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
