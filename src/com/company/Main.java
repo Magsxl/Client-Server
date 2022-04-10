2f424253d78c172506cc0280e13c6a0c873a12c0
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-10 15:59:11 +0200

    Add InputStream and OutputStream. Add response from server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,13 @@ public class Main {
     public static void Connection () {
         try(ServerSocket serverSocket = new ServerSocket(2000)) {
             Socket connSocket = serverSocket.accept();
+
+            InputStream inputStream = connSocket.getInputStream();
+            OutputStream outputStream = connSocket.getOutputStream();
+
+            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"),true);
+
+            printWriter.println("Połączono");
         } catch (IOException e) {
             e.printStackTrace();
         }
