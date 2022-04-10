0b619b9a113ad7b345bad6bee6f2432f89659209
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-10 16:45:49 +0200

    Add connection on socket and simple request message

--- Klient/Program.cs
+++ Klient/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Klient
+{
+    class klient
+    {
+        public static void Main(string[] args)
+        {
+            string host = "localhost";
+            int port = 2000;
+            byte[] buffer;
+            try
+            {
+                TcpClient client = new TcpClient(host,port);
+                NetworkStream network = client.GetStream();
+                buffer = new byte[client.ReceiveBufferSize];
+                int length = network.Read(buffer, 0, client.ReceiveBufferSize);
+                String time = Encoding.ASCII.GetString(buffer);
+                Console.WriteLine("Na {0} jest:{1}", host, time.Substring(0,length));
+            } catch
+            {
+                Console.WriteLine("Wyjątek!");
+            }
+        }
+    }
+}
