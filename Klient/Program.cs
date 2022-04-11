b9c29d1c784c1541ebb41a4f28d8fbab87fb8f42
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-11 21:26:27 +0200

    Add receiving message in client

--- Klient/Program.cs
+++ Klient/Program.cs
@@ -19,19 +19,16 @@ namespace Klient
                     soc.Connect(remoteEP);
                     String msg = Console.ReadLine();
                     byte[] byData = Encoding.ASCII.GetBytes(msg);
-                    soc.Send(byData);
-                    
-                    /* byte[] buffer = new byte[1024];
-                    int iRx = soc.Receive(buffer);
-                    char[] chars = new char[iRx];
-
-                    Decoder d = Encoding.UTF8.GetDecoder();
-                    int charLen = d.GetChars(buffer, 0, iRx, chars, 0);
-                    String recv = new String(chars); */
-
+                    int byteCount = soc.Send(byData, 0, byData.Length, SocketFlags.None);
+                    byData = new Byte[1024]; // Add receiving message
+                    String responseData = String.Empty;
+                    byteCount = soc.Receive(byData, 0, byData.Length, SocketFlags.None);
+                    if (byteCount > 0)
+                    {
+                        Console.WriteLine(Encoding.UTF8.GetString(byData, 0, byteCount));
+                    }
                     soc.Disconnect(false);
                     soc.Close();
-
                 }
             } catch
             {
