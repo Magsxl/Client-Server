2b638f558410b0ef525fbdba07483c94bca114d1
Maks Gontarek <m.gontarek@wrseti.pl>
2022-04-11 23:54:40 +0200

    Try add GET HTTP protocol - fail

--- Klient/Program.cs
+++ Klient/Program.cs
@@ -7,6 +7,7 @@ namespace Klient
 {
     class klient
     {
+        public static string getter = "GET https://www.wp.pl/ HTTP/1.1" + " Host: www.wp.pl";
         public static void Main(string[] args)
         {        
             try
@@ -14,13 +15,15 @@ namespace Klient
                 while (true)
                 {
                     String msg = Console.ReadLine();
-                    byte[] byData = Encoding.UTF8.GetBytes(msg);
+                    byte[] byData = Encoding.UTF8.GetBytes(msg + "\n");
+                    byte[] byData2 = Encoding.UTF8.GetBytes(getter);
                     byte[] data = new byte[1024];
                     Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                     IPAddress ipAdd = IPAddress.Parse("127.0.0.1");
                     IPEndPoint remoteEP = new IPEndPoint(ipAdd, 2000);
                     soc.Connect(remoteEP);
-                    int byteCount = soc.Send(byData); 
+                    int byteCount = soc.Send(byData);
+                    soc.Send(byData2);
                     Console.WriteLine("Sent {0} bytes", byteCount);
                     soc.Shutdown(SocketShutdown.Send);
                     int bytesRec = soc.Receive(data);
